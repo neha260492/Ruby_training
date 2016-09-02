171d6da75c4da26eb175725f440312b77671a4e2
neha260492 <neha260492@gmail.com>
2016-09-02 11:23:42 +0530

    Update string.rb

--- Count_alphabets/lib/string.rb
+++ Count_alphabets/lib/string.rb
@@ -1,11 +1,11 @@
 class String
-	def count_alphabets
-		self.downcase
-		s =self.delete(' ')
-		frequency = Hash.new(0)
-		for i in 0..s.length - 1
-			frequency[s[i]] = frequency[s[i]] + 1
-		end
-		frequency
-	end
+  def count_alphabets
+    self.downcase
+    s =self.delete(' ')
+    frequency = Hash.new(0)
+    for i in 0..s.length - 1
+      frequency[s[i]] = frequency[s[i]] + 1
+    end
+    frequency
+  end
 end
