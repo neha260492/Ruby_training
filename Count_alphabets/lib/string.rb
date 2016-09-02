dcd99525d467b2cb462a61f9634ac5fc9fb744ac
Neha Agrawal <neha.agrawal@pstakecare.com>
2016-09-02 11:20:18 +0530

    .

--- Count_alphabets/lib/string.rb
+++ Count_alphabets/lib/string.rb
@@ -1,5 +1,4 @@
 class String
-<<<<<<< HEAD
 	def count_alphabets
 		self.downcase
 		s =self.delete(' ')
@@ -9,16 +8,4 @@ class String
 		end
 		frequency
 	end
-=======
-  def count_alphabets
-    self.downcase
-    frequency = Hash.new(0)
-    for i in ('a'..'z')
-      if self.count(i) > 0
-        frequency[i] = self.count(i)
-      end
-    end
-    frequency
-  end
->>>>>>> 3854f5f327877c09b8d59e0d1655dd958a5647b2
 end
