0cd12fada5a4a66d211661f1d713a2069b8bff7b
Neha Agrawal <neha.agrawal@pstakecare.com>
2016-09-02 17:22:24 +0530

    .

---
+++ Palindrome/bin/main.rb
@@ -0,0 +1,4 @@
+require_relative "../lib/string"
+
+line = gets.chomp
+line.palindrome
---
+++ Palindrome/lib/string.rb
@@ -0,0 +1,10 @@
+class String
+  def palindrome
+    downcase
+    if self == self.reverse
+    	puts "#{self} is a palindrome"
+    else
+    	puts "#{self} is not a palindrome"
+    end
+  end
+end
