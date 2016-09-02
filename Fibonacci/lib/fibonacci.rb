2e99c90039e3fee7abe677b0c8170ec69e9dd3aa
Neha Agrawal <neha.agrawal@pstakecare.com>
2016-09-02 16:05:44 +0530

    .

--- Fibonacci/lib/fibonacci.rb
+++ Fibonacci/lib/fibonacci.rb
@@ -1,16 +1,15 @@
 class Fibonacci
   def call_block
-  	a = 1
-  	b = 1
-  	yield a
-  	while b <= 1000
-  		yield b
-  		b = a + b
-  	  a = b - a
-  	end
+  a = 1
+  b = 1
+  yield a
+  while b <= 1000
+  	yield b
+  	b = a + b
+    a = b - a
+  end
   end
 end
-
 
 
   
