c3db173423ba38520969769a415b7f97e4b23e07
Neha Agrawal <neha.agrawal@pstakecare.com>
2016-09-02 17:48:41 +0530

    .

--- Palindrome/bin/main.rb
+++ Palindrome/bin/main.rb
@@ -2,7 +2,7 @@ require_relative "../lib/string"
 
 line = gets.chomp
 while (line != "q") && (line != "Q")
-	line.palindrome
-	line = gets.chomp
+  line.palindrome
+  line = gets.chomp
 end
 puts "end"
