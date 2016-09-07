1e7a99079905227be0e5403384b53da3491ef7b5
Neha Agrawal <neha.agrawal@pstakecare.com>
2016-09-07 16:31:45 +0530

    exercise 20

--- interest_diff_calculator/bin/main.rb
+++ interest_diff_calculator/bin/main.rb
@@ -4,9 +4,9 @@ puts "enter principal and time(in years)"
 principal = gets.chomp
 time = gets.chomp
 interest = Interest.new do |interest|
-	interest.principal = principal.to_i
-	interest.time = time.to_f
-	interest.rate = 10
+  interest.principal = principal.to_i
+  interest.time = time.to_f
+  interest.rate = 10
 end
 # interest = Interest.new(principal.to_i, time.to_f)
 puts "Difference: #{interest.difference}"
